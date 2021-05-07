1a04d661b8c8c03df73ac4ceb6fbe9c7c19656b3
Vasiliy Golban <vgolban@accesssoftek.com>
2021-05-07 11:40:42 +0300

    Add MethodSource with method static Stream<Arguments>

---
+++ src/test/java/pages/SearchData.java
@@ -0,0 +1,23 @@
+package pages;
+
+public class SearchData {
+    private String word1;
+
+    private int word2;
+
+    public String getWord1() {
+        return word1;
+    }
+
+    public void setWord1(String word1) {
+        this.word1 = word1;
+    }
+
+    public int getWord2() {
+        return word2;
+    }
+
+    public void setWord2(int word2) {
+        this.word2 = word2;
+    }
+}
--- src/test/java/tests/AppTests.java
+++ src/test/java/tests/AppTests.java
@@ -1,14 +1,20 @@
 package tests;
 
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import pages.SearchData;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.exist;
 
-public class AppTests extends BaseTest{
+public class AppTests extends BaseTest {
 
     @Test
     @DisplayName("Menu sections presented")
@@ -42,5 +48,24 @@ public class AppTests extends BaseTest{
     void SearchWithCsv(String word1, int word2) {
         homePage.searchField.setValue(word1);
         homePage.searchField.setValue(String.valueOf(word2));
+    }
+
+    @MethodSource // if Stream<Arguments> method has other name, we need specify it here;
+    @ParameterizedTest
+    void SearchWithMethodSource(SearchData searchData, String moreData) {
+        homePage.searchField.setValue(searchData.getWord1());
+        homePage.searchField.sendKeys(String.valueOf(searchData.getWord2()));
+        homePage.searchField.sendKeys(String.valueOf(moreData));
+        Selenide.sleep(2000);
+    }
+
+    static Stream<Arguments> SearchWithMethodSource() {
+        SearchData a =  new SearchData(); a.setWord1("hello"); a.setWord2(111);
+        SearchData b =  new SearchData(); b.setWord1("world"); b.setWord2(222);
+
+        return Stream.of(
+                Arguments.of(a, "---"),
+                Arguments.of(b, "^^^")
+        );
     }
 }
