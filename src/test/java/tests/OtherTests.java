081fb01363ad7f203d656ccda427b81afec8d0b6
Vasiliy Golban <vgolban@accesssoftek.com>
2021-05-19 16:34:14 +0300

    Assert exception is thrown

--- src/test/java/tests/OtherTests.java
+++ src/test/java/tests/OtherTests.java
@@ -1,6 +1,20 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class OtherTests {
 
+    int MyDoublerMethod(int num) {
+        if (num == 0) {
+            throw new IllegalArgumentException();
+        }
+        return num * 2;
+    }
 
+    @Test
+    void ExceptionIsThrown() {
+        assertThrows(IllegalArgumentException.class, () -> MyDoublerMethod(0));
+    }
 }
