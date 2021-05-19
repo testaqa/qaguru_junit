ad92402ca7f21d97d2bee64d8ede009d7c50d24f
Vasiliy Golban <vgolban@accesssoftek.com>
2021-05-19 16:45:16 +0300

    Create temp directory during execution

--- src/test/java/tests/OtherTests.java
+++ src/test/java/tests/OtherTests.java
@@ -1,8 +1,14 @@
 package tests;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class OtherTests {
 
@@ -16,5 +22,15 @@ public class OtherTests {
     @Test
     void ExceptionIsThrown() {
         assertThrows(IllegalArgumentException.class, () -> MyDoublerMethod(0));
+    }
+
+    @TempDir
+    Path tempDir;
+
+    @Test
+    void TempDirForTest() throws IOException {
+        Path createdFile = Files.createFile(tempDir.resolve("createdFile.txt"));
+
+        assertTrue(createdFile.toFile().exists());
     }
 }
